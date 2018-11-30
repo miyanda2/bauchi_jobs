cccd76bf014b4af7fb17301b1c9b0f2c11ae753f
micheal <micheal@uplift.ng>
2018-11-30 03:30:46 +0100

    Create User Permission!!!!!!!

--- Quiz/admin.py
+++ Quiz/admin.py
@@ -1,4 +1,15 @@
 from django.contrib import admin
 from .models import Quiz, Score
 
+from django.http import HttpResponse, HttpResponseRedirect
+
+from django.contrib.auth.admin import UserAdmin
+
+
+
+
+
+
+
+
 admin.site.register(Quiz)
--- Quiz/forms.py
+++ Quiz/forms.py
@@ -9,6 +9,8 @@ class RegisterForm(UserCreationForm):
 	last_name = forms.CharField(max_length=30, required=True, help_text='Required.')
 	email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
 	school = forms.CharField(max_length=30, required=True, help_text='Required.')
+	Phone_num = forms.IntegerField(required=True, help_text='Required.')
+	CV = forms.FileField()
 
 
 
@@ -17,7 +19,8 @@ class RegisterForm(UserCreationForm):
 		fields = [  'username',
 					'first_name',
 					'last_name',
-					'school'
+					'Phone_num',
+					'school',
 					
 	                    
 	            ]
